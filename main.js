04afea61c121987d337918c6f61118bb3d1a4eef
xCode4lifex <141378661+xCode4lifex@users.noreply.github.com>
2024-02-15 19:23:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot() {
+    save('Clown.png');
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nosex ="+ results[0].pose.nose.x);
+        console.log("nosey ="+ results[0].pose.nose.y);
+    }
+}
